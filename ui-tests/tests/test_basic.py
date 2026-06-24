a4d50bb6372b0335673ddaf9f33c502854c8b01e
Livia Banyai <livia5banyai@gmail.com>
2026-06-24 17:39:57 +0100

    testing homepage title rendering

---
+++ ui-tests/tests/test_basic.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import sync_playwright
+
+def test_title():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+        page.goto("https://www.saucedemo.com/")
+        assert "Swag Labs" in page.title()
+        browser.close()
