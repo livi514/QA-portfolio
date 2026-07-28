63044a6f5bcd0399f927f148d878d69e8a5565b7
Livia Banyai <livia5banyai@gmail.com>
2026-07-28 16:36:57 +0100

    test_performance.py updating threshold

--- week-4-api-testing/tests/test_performance.py
+++ week-4-api-testing/tests/test_performance.py
@@ -16,12 +16,12 @@ BASE_URL = "https://jsonplaceholder.typicode.com"
 )
 def test_response_time(endpoint):
     """
-    Test that the response time of crucial endpoints does not exceed one second.
-    1.0 second is a reasonable threshold for a public API under normal conditions.
+    Test that the response time of crucial endpoints does not exceed 1.5.
+    1.5 seconds is a reasonable threshold for a public API under normal conditions.
     On a real project this threshold would be defined in performance requirements
     """
 
     response = requests.get(f"{BASE_URL}{endpoint}")
     assert (
-        response.elapsed.total_seconds() < 1.0
+        response.elapsed.total_seconds() < 1.5
     ), f"{endpoint} took {response.elapsed.total_seconds():.2f}s"
