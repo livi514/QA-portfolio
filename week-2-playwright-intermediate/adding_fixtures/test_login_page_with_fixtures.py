f743d6962d295e7b185718d64e9113247f01e9a8
livi514 <livia5banyai@gmail.com>
2026-09-07 17:26:08 +0100

    fix: typo in test_response_code() comment

--- week-2-playwright-intermediate/adding_fixtures/test_login_page_with_fixtures.py
+++ week-2-playwright-intermediate/adding_fixtures/test_login_page_with_fixtures.py
@@ -89,7 +89,7 @@ def test_login_button_text(page):
 
 
 def test_response_code(page):
-    # The page should load correcly with the response code 200 OK.
+    # The page should load correctly with the response code 200 OK.
 
     # navigate to the login page
     response = page.goto("https://www.saucedemo.com/")
