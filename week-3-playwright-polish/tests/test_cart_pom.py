46b15fb34776a900ced1ddde70125c869a4a52d6
livi514 <livia5banyai@gmail.com>
2026-09-07 16:52:12 +0100

    improve comments for clarity

--- week-3-playwright-polish/tests/test_cart_pom.py
+++ week-3-playwright-polish/tests/test_cart_pom.py
@@ -17,10 +17,10 @@ def test_empty_cart_state(log_in_to_saucedemo):
 def test_add_item_to_cart(add_backpack_to_cart):
     # This test verifies that an item can be added to the cart and that the cart displays the correct information.
 
-    # check number of items in the cart
+    # check the number of items in the cart
     cart = CartPage(add_backpack_to_cart)
     assert cart.get_item_count() == 1
-    # check name of item
+    # check the name of the item
     assert cart.get_item_names()[0] == "Sauce Labs Backpack"
     # check the text on the 'Remove' button
     expect(cart.remove_backpack_button).to_have_text("Remove")
@@ -36,13 +36,13 @@ def test_remove_item_from_cart(add_backpack_to_cart):
     assert cart.get_item_count() == 0
 
 
-def test_removing_item_doesnt_affect_others(add_backpack_and_bike_light_to_cart):
+def test_removing_item_does_not_affect_others(add_backpack_and_bike_light_to_cart):
     # This test verifies that removing one item from the cart does not affect other items in the cart.
 
-    # check number of items in the cart
+    # check the number of items in the cart
     cart = CartPage(add_backpack_and_bike_light_to_cart)
     assert cart.get_item_count() == 2
-    # check name of items
+    # check the names of the items
     assert cart.get_item_names()[0] == "Sauce Labs Backpack"
     assert cart.get_item_names()[1] == "Sauce Labs Bike Light"
     # click on 'Remove' button for the backpack
@@ -68,7 +68,7 @@ def test_persistence_with_continue_shopping_button(add_backpack_and_bike_light_t
     # This test verifies that clicking the 'Continue Shopping' button does not remove items from the cart.
 
     cart = CartPage(add_backpack_and_bike_light_to_cart)
-    # check number of items in the cart
+    # check the number of items in the cart
     assert cart.get_item_count() == 2
     # click on the 'Continue Shopping' button
     cart.continue_shopping()
@@ -77,7 +77,7 @@ def test_persistence_with_continue_shopping_button(add_backpack_and_bike_light_t
     inventory.open_cart()
     # check that the items are still in the cart
     assert cart.get_item_count() == 2
-    # check names of items
+    # check the names of the items
     assert cart.get_item_names()[0] == "Sauce Labs Backpack"
     assert cart.get_item_names()[1] == "Sauce Labs Bike Light"
 
@@ -86,7 +86,7 @@ def test_cart_persists_after_logout_and_login(add_backpack_and_bike_light_to_car
     # This test verifies that items in the cart persist after logging out and logging back in.
 
     cart = CartPage(add_backpack_and_bike_light_to_cart)
-    # check number of items in the cart
+    # check the number of items in the cart
     assert cart.get_item_count() == 2
     # log out by clicking on the menu button and then clicking 'Logout'
     menu = MenuPage(add_backpack_and_bike_light_to_cart)
