24b7caf5eb8c41e9800f4c7a7a990bac295acc52
livi514 <livia5banyai@gmail.com>
2026-09-07 17:25:37 +0100

    fix: correct function name for clarity in test_removing_item_does_not_affect_others

--- week-2-playwright-intermediate/adding_fixtures/test_cart_page_with_fixtures.py
+++ week-2-playwright-intermediate/adding_fixtures/test_cart_page_with_fixtures.py
@@ -38,7 +38,7 @@ def test_remove_item_from_cart(add_backpack_to_cart):
     expect(add_backpack_to_cart.locator(".cart_item")).to_have_count(0)
 
 
-def test_removing_item_doesnt_affect_others(add_backpack_and_bike_light_to_cart):
+def test_removing_item_does_not_affect_others(add_backpack_and_bike_light_to_cart):
     # This test verifies that removing one item from the cart does not affect other items in the cart.
 
     # check number of items in the cart
